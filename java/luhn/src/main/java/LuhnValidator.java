f537cc9b67e128a244a0de16642aebcb955ef8f1
Daniel Weibel <danielmweibel@gmail.com>
2017-05-22 16:27:06 +0700

    luhn: new iteration

--- java/luhn/src/main/java/LuhnValidator.java
+++ java/luhn/src/main/java/LuhnValidator.java
@@ -1,29 +1,28 @@
-import java.util.List;
 import java.util.stream.IntStream;
-import java.util.stream.Collectors;
 
 public class LuhnValidator {
 
   public boolean isValid(String num) {
-    num = num.replaceAll("\\s+", "");
     return isValidFormat(num) && isValidLuhn(num);
   }
 
   private boolean isValidLuhn(String num) {
-    List<Integer> d = num.chars().mapToObj(Character::getNumericValue).collect(Collectors.toList());
-    if (d.size() % 2 != 0) d.add(0, 0);
-
-    return IntStream.range(0, d.size())
-      .map(i -> i % 2 == 0 ? transDigit(d.get(i)) : d.get(i))
+    int[] d = num.chars().filter(Character::isDigit).map(Character::getNumericValue).toArray();
+    return IntStream.range(0, d.length)
+      .map(i -> isEvenFromRight(i, d) ? d[i] : transDigit(d[i]))
       .sum() % 10 == 0;
   }
 
   private boolean isValidFormat(String num) {
-    return num.matches("\\d{2,}");
+    return num.matches("(\\s*\\d\\s*){2,}");
   }
 
-  private int transDigit(int n) {
-    return n * 2 > 9 ? n * 2 - 9 : n * 2;
+  private boolean isEvenFromRight(int i, int[] a) {
+    return (a.length - 1 - i) % 2 == 0;
+  }
+
+  private int transDigit(int d) {
+    return d < 5 ? d * 2 : d * 2 - 9;
   }
 
 }
