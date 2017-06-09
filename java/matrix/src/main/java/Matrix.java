c8bc68edbed198a267c6ed1dc5358bd5e1bb44c3
Daniel Weibel <danielmweibel@gmail.com>
2017-06-09 20:20:42 +0700

    Matrix, second iteration
    Use int[][] instead of List<List<Integer>>.

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,42 +1,38 @@
-import java.util.stream.IntStream;
-import java.util.stream.Collectors;
-import java.util.List;
-import java.util.ArrayList;
-
 public class Matrix {
 
-  private List<List<Integer>> matrix = new ArrayList<>();
+  private int[][] matrix;
 
   public Matrix(String str) {
     String[] rows = str.split("\n");
+    matrix = new int[rows.length][];
 
-    for (String row : str.split("\n")) {
-      matrix.add(row.codePoints()
+    for (int i = 0; i < rows.length; i++) {
+      matrix[i] = rows[i].codePoints()
         .filter(Character::isDigit)
         .map(Character::getNumericValue)
-        .boxed()
-        .collect(Collectors.toList()));
+        .toArray();
     }
   }
 
   public int getRowsCount() {
-    return matrix.size();
+    return matrix.length;
   }
 
   public int getColumnsCount() {
-    return matrix.size() > 0 ? matrix.get(0).size() : 0;
+    return matrix.length > 0 ? matrix[0].length : 0;
   }
 
-  public int[] getRow(int index) {
-    return matrix.get(index).stream().mapToInt(x -> x).toArray();
+  public int[] getRow(int rowIndex) {
+    return matrix[rowIndex];
   }
 
-  public int[] getColumn(int index) {
+  public int[] getColumn(int colIndex) {
     int[] col = new int[getRowsCount()];
     for (int i = 0; i < getRowsCount(); i++) {
-      col[i] = matrix.get(i).get(index);
+      col[i] = matrix[i][colIndex];
     }
     return col;
   }
+
 }
 
